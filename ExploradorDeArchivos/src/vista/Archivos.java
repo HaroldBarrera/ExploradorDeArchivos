37ca553782550c23989804f91f3cdd5c12223bf3
Harold Barrera NIño <>
2022-03-22 19:08:33 -0500

    Diseño del explorador de archivos.

--- ExploradorDeArchivos/src/vista/Archivos.java
+++ ExploradorDeArchivos/src/vista/Archivos.java
@@ -8,23 +8,37 @@ public class Archivos extends ScrollPane {
 
     public Archivos(){
         //Carpetas principales
-        DefaultMutableTreeNode style=new DefaultMutableTreeNode("Style");
+        DefaultMutableTreeNode main=new DefaultMutableTreeNode("Explorador");
 
         //Carpetas secundarias
-        DefaultMutableTreeNode color=new DefaultMutableTreeNode("color");
-        DefaultMutableTreeNode font=new DefaultMutableTreeNode("font");
-        style.add(color);
-        style.add(font);
+        DefaultMutableTreeNode documents=new DefaultMutableTreeNode("Documentos");
+        DefaultMutableTreeNode images=new DefaultMutableTreeNode("Imagenes");
+        DefaultMutableTreeNode downloads=new DefaultMutableTreeNode("Descargas");
+        DefaultMutableTreeNode desktop=new DefaultMutableTreeNode("Escritorio");
+        main.add(documents);
+        main.add(images);
+        main.add(downloads);
+        main.add(desktop);
 
-        //Items de la carpeta color
+        //Items de la carpeta images
         DefaultMutableTreeNode red=new DefaultMutableTreeNode("red");
         DefaultMutableTreeNode blue=new DefaultMutableTreeNode("blue");
         DefaultMutableTreeNode black=new DefaultMutableTreeNode("black");
         DefaultMutableTreeNode green=new DefaultMutableTreeNode("green");
-        color.add(red); color.add(blue); color.add(black); color.add(green);
+        images.add(red); images.add(blue); images.add(black); images.add(green);
+
+        //Items de la carpeta documents
+        DefaultMutableTreeNode tarea=new DefaultMutableTreeNode("tarea.txt");
+        DefaultMutableTreeNode php=new DefaultMutableTreeNode("conexion.php");
+        DefaultMutableTreeNode python=new DefaultMutableTreeNode("helloworld.py");
+        documents.add(tarea); documents.add(php); documents.add(python);
+
+        //Items de la carpeta documents
+        DefaultMutableTreeNode netbeans=new DefaultMutableTreeNode("netbeans.exe");
+        downloads.add(netbeans);
 
         //Creacion del JTree
-        JTree jt=new JTree(style);
+        JTree jt=new JTree(main);
 
         add(jt);
     }
