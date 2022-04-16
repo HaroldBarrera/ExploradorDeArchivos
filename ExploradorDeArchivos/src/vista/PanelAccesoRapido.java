fe67e1f181531432756c3c1998f6e3b3ed3f8403
Harold Barrera NIño <>
2022-04-16 13:47:48 -0500

    Panel de acceso rapido
    Se le agrego un diseño al panel de acceso rapido ademas de una funcionalidad para abrir las configuraciones de pc.

--- ExploradorDeArchivos/src/vista/PanelAccesoRapido.java
+++ ExploradorDeArchivos/src/vista/PanelAccesoRapido.java
@@ -2,11 +2,41 @@ package vista;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class PanelAccesoRapido extends JPanel {
 
     public PanelAccesoRapido(){
-        setBackground(Color.CYAN);
+        setBackground(Color.WHITE);
+        setLayout(null);
+
+        JLabel c1 = new JLabel("Documentos");
+        c1.setIcon(new ImageIcon("carpeta.png"));
+        c1.setBounds(50, 50, 250, 250);
+
+        JLabel c2 = new JLabel("Imagenes");
+        c2.setIcon(new ImageIcon("carpeta.png"));
+        c2.setBounds(350, 50, 250, 250);
+
+        JLabel c3 = new JLabel("Descargas");
+        c3.setIcon(new ImageIcon("carpeta.png"));
+        c3.setBounds(50, 250, 250, 250);
+
+        JLabel computador = new JLabel("PC");
+        computador.setIcon(new ImageIcon("computador.png"));
+        computador.setBounds(350, 250, 250, 250);
+        computador.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                new ConfiguracionDelPC();
+            }
+        });
+
+        add(c1);
+        add(c2);
+        add(c3);
+        add(computador);
     }
 
 }
